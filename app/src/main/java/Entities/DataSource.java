1f5c0b5546d8ede6dc44e2b88c7b8c7206b3b517
MisterX25 <xavier.carrel@cpnv.ch>
2016-06-15 10:12:38 +0200

    Cleaned up imports

--- app/src/main/java/Entities/DataSource.java
+++ app/src/main/java/Entities/DataSource.java
@@ -1,41 +1,11 @@
 package Entities;
 
 import android.content.Context;
-import android.os.AsyncTask;
-import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.RequestFuture;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import ch.cpnv.cpnvsports.R;
 
 /**
  * Dummy class whos only purpose is to provide hardcoded application data
